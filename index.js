df5db51db18843eca8b8dec216f34867e48de7a0
codewithirene567 <irenejinscott@gmail.com>
2020-08-19 20:50:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+
+    const reduce = function(sourceArray, reducer, total = sourceArray[0]) { 
+        if (total != sourceArray[0]) {
+            sourceArray.forEach((element) => {
+                total = reducer(element, total) //total, because it is reducing it to a total value
+            })
+        } else {
+            for (let i = 1; i < sourceArray.length; i++) { // if initial value which is sourceArray[0] then run else
+                total = reducer(sourceArray[i], total)
+            }
+        }
+        return total
+    }
+
+
+function map(sourceArray, afunction){
+    const fullArray = []
+    for (const element in sourceArray) { // for each element in the source array
+        fullArray.push(afunction(sourceArray[element])) // put the elements from the source array
+        //which are functions into the empty array
+    }
+    return fullArray
+ }
+
+
